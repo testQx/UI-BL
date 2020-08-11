5e9337f1994f80dffbc88d3e7a26fce9684dc27a
textQx <476349477@qq.com>
2020-08-12 00:31:14 +0800

    1.2版本，修复测试报告曲线图

---
+++ demo_test/demo_thread.py
@@ -0,0 +1,32 @@
+
+# class Test():
+#     @staticmethod
+#     def wrapper(func):
+#         def f():
+#             print('在wrapper中执行func前')
+#             func()
+#             print('在wrapper中执行func后')
+#         return f
+#
+# @Test.wrapper
+# def fengshan():
+#     print('出风')
+#
+# fengshan()
+from Common.Publicmethod import Publicmethod
+
+@Publicmethod.thread
+def fengshan(i):
+    #若threading.Thread(target=func,args=(args[0],))
+    # 则该处fengshan()需更改为 fengshan(loops)需要接收参数
+    print('出风')
+
+if __name__ == '__main__':
+    fengshan(count=4)
+#[1,2,3,4]进入thread中的*arg中
+
+'''>>>
+    出风
+    出风
+    出风
+    出风'''
